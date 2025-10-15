3d1fbef10b9eda824ca19b765e0226f9b529598d
Arshad Khan <ArshadKhan627@outlook.com>
2025-10-15 12:58:06 +0530

    Initial Commit

--- 14_clousers/main.go
+++ 14_clousers/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+//Clousers are functions that capture the surrounding state or environment in which they were created
+//In Go, closures are implemented using anonymous functions that can access variables from their enclosing scope
+func counter() func() int { //here, counter returns a function that returns an integer
+	var count int = 0
+	return func() int { //Here, an anonymous function is returned that increments and returns the count variable
+		count++
+		return count
+	}
+}
+
+func main() {
+	increment := counter() //Here, increment is a closure that captures the count variable from the counter function
+	fmt.Println(increment())
+	fmt.Println(increment())
 }
