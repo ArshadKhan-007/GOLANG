7ae231628e22ccab692e64c96937fecd5deb9aec
Arshad Khan <ArshadKhan627@outlook.com>
2025-10-15 13:21:16 +0530

    Initial Commit

--- 14_clousers/main.go
+++ 14_clousers/main.go
@@ -12,8 +12,22 @@ func counter() func() int { //here, counter returns a function that returns an i
 	}
 }
 
+func makeadder() func(int) int {
+	var sum int = 0
+	return func(x int) int {
+		sum += x
+		return sum
+
+	}
+}
+
 func main() {
 	increment := counter() //Here, increment is a closure that captures the count variable from the counter function
 	fmt.Println(increment())
 	fmt.Println(increment())
+
+	adder := makeadder()
+	fmt.Println(adder(5))
+	fmt.Println(adder(10))
+	fmt.Println(adder(20))
 }
