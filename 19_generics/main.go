3e3bf43ffb88d9967877348d57d0a10fdf8db1f8
Arshad Khan <ArshadKhan627@outlook.com>
2025-10-18 20:09:19 +0530

    Initial Commit

--- 19_generics/main.go
+++ 19_generics/main.go
@@ -6,10 +6,24 @@ import "fmt"
 // In simple terms, generics enable you to create reusable components that can work with any data type.
 
 // Generic Function Example
+// This function can take a slice of any type and print its elements
 func printSlice[T any](items []T) { // T is a type parameter that can be any type
 	for _, item := range items {
 		fmt.Println(item)
 	}
+}
+
+// Function specifically for int type and string type
+// This function will only take slices of int or string types
+func printAnotherSlice[T int | string](items []T) { // T is constrained to int or string types
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
+// Struct with Generics Example
+type Pair[T any] struct {
+	elements []T
 }
 
 func main() {
@@ -20,6 +34,21 @@ func main() {
 	printSlice(intSlice)
 	fmt.Println("-----")
 	printSlice(stringSlice)
+	fmt.Println("-----")
+	printAnotherSlice(intSlice)
+	fmt.Println("-----")
+	printAnotherSlice(stringSlice)
+	fmt.Println("----------")
+
+	// Using the generic struct with different types
+	p := Pair[int]{
+		elements: []int{1, 24, 5},
+	}
+	fmt.Println(p)
+	x := Pair[string]{
+		elements: []string{"a", "b", "c"},
+	}
+	fmt.Println(x)
 }
 
 // As we can see in the above code example, the function printSlice is defined with a type parameter T.
